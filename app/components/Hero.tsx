dca6f1bbef06a11d12e2af6e37cc154641f7cb3e
TanishqKBhardwaj <kvbharwaj4@email.com>
2025-02-20 23:38:02 +0530

    Day 2

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -1,13 +1,18 @@
 import React from 'react'
 import { cn } from "@/lib/utils";
 import { Spotlight } from "../ui/Spotlight";
+import { TextGenerateEffect } from '../ui/text-generate-effect';
  
 function Hero() {
   return (
-    <div className="max-h-lvh md:max-h-fit w-full rounded-md flex md:items-center md:justify-center bg-black/[0.96] antialiased bg-grid-white/[0.02] relative overflow-hidden">
+    <div className="max-h-lvh mt-10   md:h-[80vh] w-full rounded-md flex md:items-center md:justify-center antialiased bg-grid-white/[0.02] relative overflow-hidden" >
+      
+
+      <div className="h-full w-full bg-black  dark:bg-grid-white/[0.05] bg-grid-black/[0.2] relative flex items-center justify-center" style={{backgroundColor:'#070F2B'}}>
+     
       <Spotlight
         className=" absolute -top-40 left-0 md:right-0 md:top-0"
-        fill="green"
+        fill="blue"
       />
 
 <Spotlight
@@ -15,16 +20,16 @@ function Hero() {
         fill="white"
       />
       <div className=" p-4 max-w-7xl  mx-auto relative z-10  w-full pt-20 md:pt-0">
-        <h1 className="text-4xl md:text-7xl font-bold text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 bg-opacity-50">
-          Spotlight <br /> is the new trend.
-        </h1>
-        <p className="mt-4 font-normal text-base text-neutral-300 max-w-lg text-center mx-auto">
-          Spotlight effect is a great way to draw attention to a specific part
-          of the page. Here, we are drawing the attention towards the text
-          section of the page. I don&apos;t know why but I&apos;m running out of
-          copy.
-        </p>
+        
+       <TextGenerateEffect className='text-center text-orange-500' duration={2} filter={false} textSize='text-4xl md:text-7xl' words="Hi," />
+       <br/>
+       <TextGenerateEffect className='text-center' duration={2} filter={false} textSize='text-4xl md:text-7xl' words="I am Tanishq Bhardwaj" />
+
+       
+        <TextGenerateEffect className='text-center' textSize='text-xl md:text-2xl' duration={2} filter={true} words="Aspiring Software Developer specializing in modern web development. Lets build something amazing together!"/>
+       
       </div>
+    </div>
     </div>
 )}
 
